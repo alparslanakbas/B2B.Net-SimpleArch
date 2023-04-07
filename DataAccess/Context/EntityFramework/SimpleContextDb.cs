244806a9bd456b24f88be650191e81a501565c53
AlparslanTR <bybluestht@gmail.com>
2023-04-07 21:21:13 +0300

    Proje dosyası ekle.

---
+++ DataAccess/Context/EntityFramework/SimpleContextDb.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Context.EntityFramework
+{
+    public class SimpleContextDb : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-38ER4CK\\SQLEXPRESS;Initial Catalog=B2BDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+        public DbSet<EmailParameter> EmailParameters { get; set; }
+    }
+}
